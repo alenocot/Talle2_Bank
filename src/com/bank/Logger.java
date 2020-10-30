3a3f652d1a92e73fb73df60edc2f1110f5d2971d
Tommy Burgos <trburgos@espol.edu.ec>
2020-10-29 23:13:51 -0500

    Cambios en el logger

--- src/com/bank/Logger.java
+++ src/com/bank/Logger.java
@@ -8,32 +8,33 @@ import java.util.*;
 
 public aspect Logger {
 
-	pointcut successT() : call(* moneyMakeTransaction*(..) );    
-	after() : successT() {
+	pointcut success() : call(* money*(..) );    
+	after() : success() {
+		
     	try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File("log.txt"),true))){
-    		Calendar cal = new GregorianCalendar();
-    		String fecha = Calendar.HOUR_OF_DAY+":"+Calendar.MINUTE+":"+Calendar.SECOND+":"+Calendar.MILLISECOND;
-    		bw.append("transaccion: "+fecha);
-    		System.out.println("Transaction: "+fecha);    		
-    		bw.newLine();
+    		String var="transaction";
+    		if(thisJoinPointStaticPart.getSignature().getName().equals("moneyMakeTransaction")) {
+    			Calendar cal = new GregorianCalendar();
+        		String fecha = Calendar.HOUR_OF_DAY+":"+Calendar.MINUTE+":"+Calendar.SECOND+":"+Calendar.MILLISECOND;
+        		bw.append("transaccion: "+fecha);
+        		System.out.println("Transaction: "+fecha);    		
+        		bw.newLine();
+        		}else {
+        			try(BufferedWriter bw2 = new BufferedWriter(new FileWriter(new File("log.txt"),true))){
+        	    		SimpleDateFormat dateFormat  = new SimpleDateFormat("HH:mm:ss");
+        	    		Date date = new Date();    		
+        	    		bw.append("Withdrawal: "+dateFormat.format(date));
+        	    		System.out.println("Withdrawal: "+dateFormat.format(date));    		
+        	    		bw.newLine();
+        	    		}catch(Exception e){
+        	    			System.out.println("Exception: "+e);
+        	    		}        
+        	    	System.out.println("** Withdrawal created **");
+        		}        		        
     		}catch(Exception e){
-    			System.out.println("Exception: "+e);
-    		}        
-    	System.out.println("** Transaction created **");
-	}
-    
-    pointcut successR() : call(* moneyWithdrawal*(..) );    
-    after() : successR() {
-    	try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File("log.txt"),true))){
-    		SimpleDateFormat dateFormat  = new SimpleDateFormat("HH:mm:ss");
-    		Date date = new Date();    		
-    		bw.append("Withdrawal: "+dateFormat.format(date));
-    		System.out.println("Withdrawal: "+dateFormat.format(date));    		
-    		bw.newLine();
-    		}catch(Exception e){
-    			System.out.println("Exception: "+e);
-    		}        
-    	System.out.println("** Withdrawal created **");
-    }
+        			System.out.println("Exception: "+e);
+        			System.out.println("** Transaction created **");
+        		}	
+    		}	    
     
 }
